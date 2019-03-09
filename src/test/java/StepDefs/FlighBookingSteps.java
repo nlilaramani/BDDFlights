4f9d067d608eac1af5cec42157b78d1bf0a29235
Owner <Owner@DESKTOP-E24TOP9>
2019-03-09 02:02:00 -0500

    Merge origin/master
    Conflicts:
    	pom.xml
    	src/test/java/features/calculator.feature

--- src/test/java/StepDefs/FlighBookingSteps.java
+++ src/test/java/StepDefs/FlighBookingSteps.java
@@ -6,9 +6,13 @@
 package StepDefs;
 
 import com.bdd.sel.test.SeleniumUtils;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.cucumber.datatable.DataTable;
+import java.util.List;
+import junit.framework.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -30,8 +34,63 @@ public class FlighBookingSteps {
         driver.findElement(By.name("password")).sendKeys("mercury");
         driver.findElement(By.name("login")).click();
     }
-     @Then("User Profile Page is Displayed")
+     @Then("Flight Search Page is displayed")
     public void validateProfilePage(){
+        String title=driver.getTitle();
+        String expTitle="Find a Flight: Mercury Tours:";
+        Assert.assertEquals(expTitle, title);
     }
-    
+    @Given("User is logged in")
+    public void userIsLoggedIn(){
+        navigateToHomePage();
+        performLogin();
+    }
+    @When("User enters following search criteria")
+    public void enterSearchCriteria(DataTable dt){
+        List<String> data=dt.asList();
+        if(data.get(0).equalsIgnoreCase("roundtrip")){
+            driver.findElements(By.name("tripType")).get(0).click();
+        }else{
+            driver.findElements(By.name("tripType")).get(1).click();   
+        }
+        driver.findElement(By.name("passCount")).sendKeys(data.get(1));
+        driver.findElement(By.name("fromPort")).sendKeys(data.get(2));
+        driver.findElement(By.name("fromMonth")).sendKeys(data.get(3));
+        driver.findElement(By.name("fromDay")).sendKeys(data.get(4));
+        driver.findElement(By.name("toPort")).sendKeys(data.get(5));
+        driver.findElement(By.name("toMonth")).sendKeys(data.get(6));
+        driver.findElement(By.name("toDay")).sendKeys(data.get(7));
+        if(data.get(8).equalsIgnoreCase("economy")){
+            driver.findElements(By.name("servClass")).get(0).click();
+        }else{
+            driver.findElements(By.name("servClass")).get(1).click();
+        }
+        driver.findElement(By.name("findFlights")).click();
+    }
+    @Then("matching flights are displayed")
+    public void verifyFlightResults(){
+        Assert.assertEquals("Select a Flight: Mercury Tours", driver.getTitle());
+    }
+    @When("User selects following flights")
+    public void selectFlights(DataTable dt){
+        List<String> data=dt.asList();
+        driver.findElements(By.name("outFlight")).get(Integer.parseInt(data.get(0))).click();
+        driver.findElements(By.name("inFlight")).get(Integer.parseInt(data.get(1))).click();
+        driver.findElement(By.name("reserveFlights")).click();
+   
+    }
+    @And("User enters following passenger details")
+     public void enterPassengerDetails(DataTable dt){
+         List<String> data=dt.asList();
+         driver.findElement(By.name("passFirst0")).clear();
+         driver.findElement(By.name("passFirst0")).sendKeys(data.get(0));
+         driver.findElement(By.name("passLast0")).sendKeys(data.get(1));
+         driver.findElement(By.name("creditnumber")).sendKeys(data.get(2));
+         driver.findElement(By.name("buyFlights")).click();
+         
+     }
+     @Then("System displays flight confirmation")
+     public void verifyConfirmation(){
+         Assert.assertEquals("Flight Confirmation: Mercury Tours", driver.getTitle());
+     }
 }
--- src/test/java/features/flightbooking.feature
+++ src/test/java/features/flightbooking.feature
@@ -6,4 +6,22 @@ Feature: Flight Bookings
 Scenario: User login
 Given User is on Home Page
 When User Logs in
-Then User Profile Page is Displayed   
+Then Flight Search Page is displayed   
+
+Scenario: Flight Search
+Given User is logged in
+When User enters following search criteria
+|roundtrip|1|New York|March|15|London|March|24|Economy|
+Then matching flights are displayed
+
+Scenario: Book Flight
+Given User is on Home Page
+When User Logs in
+And User enters following search criteria
+|roundtrip|1|New York|March|15|London|March|24|Economy|
+And User selects following flights
+|2|2|
+And User enters following passenger details
+|Narendra|Lilaramani|123412341234|
+Then System displays flight confirmation
+
